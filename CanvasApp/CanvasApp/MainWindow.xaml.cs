2cdbac5d03e6ab9b9b4b3c4ae83f3242c2b94271
Alicja Winnicka <alicja.winnicka@q-perior.com>
2023-03-13 11:46:19 +0100

    dzialajace wszystko, oby

--- CanvasApp/CanvasApp/MainWindow.xaml.cs
+++ CanvasApp/CanvasApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Printing;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,8 @@ namespace CanvasApp
         private Point currentPoint = new Point();
         private Brush newColor = new SolidColorBrush();
         private bool drawLine = false;
+        private bool startOfNewLine = false;
+        private Line straightLine = new Line();
         private List<int> strokeThicknesses = new List<int>() { 1, 5, 8, 10, 15, 20, 50 };
 
         public MainWindow()
@@ -30,43 +33,92 @@ namespace CanvasApp
             {
                 StrokeThicknesessComboBox.Items.Add(x);
             }
+            StrokeThicknesessComboBox.SelectedItem = strokeThicknesses[0];
         }
 
         private void SetDrawingLine(object sender, RoutedEventArgs e)
         {
             drawLine = !drawLine;
+            startOfNewLine = false;
         }
 
         private void Canvas_MouseDown_1(object sender, MouseButtonEventArgs e)
         {
             if (e.ButtonState == MouseButtonState.Pressed)
+            {
                 currentPoint = e.GetPosition(this);
+            }
+
+            if (drawLine)
+            {
+                if (e.LeftButton == MouseButtonState.Pressed && !startOfNewLine)
+                {
+                    straightLine = new Line();
+                    straightLine.Stroke = newColor;
+                    straightLine.StrokeThickness = Convert.ToDouble(StrokeThicknesessComboBox.SelectedItem.ToString());
+                    straightLine.X1 = currentPoint.X;
+                    straightLine.Y1 = CorrectCurrentY(currentPoint.Y);
+                    startOfNewLine = true;
+                }
+                else if (e.LeftButton == MouseButtonState.Pressed && startOfNewLine)
+                {
+                    straightLine.X2 = currentPoint.X;
+                    straightLine.Y2 = CorrectCurrentY(currentPoint.Y);
+                    startOfNewLine = false;
+                    paintSurface.Children.Add(straightLine);
+                }
+            }
+            else
+            {
+                Ellipse circle = new Ellipse();
+                circle.Width = Convert.ToDouble(StrokeThicknesessComboBox.SelectedItem.ToString());
+                circle.Height = Convert.ToDouble(StrokeThicknesessComboBox.SelectedItem.ToString());
+                Canvas.SetLeft(circle, currentPoint.X - Math.Floor(circle.Width / 2));
+                Canvas.SetTop(circle, CorrectCurrentY(currentPoint.Y) - Math.Floor(circle.Height / 2));
+                SetShapeProperties(circle);
+                circle.Fill = newColor;
+
+                paintSurface.Children.Add(circle);
+            }
         }
 
         private void Canvas_MouseMove_1(object sender, MouseEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            if (!drawLine)
             {
-                Line line = new Line();
-                line.Stroke = newColor;
-                line.StrokeThickness = Convert.ToDouble(StrokeThicknesessComboBox.SelectedItem.ToString());
-                line.X1 = currentPoint.X;
-                line.Y1 = currentPoint.Y;
-                line.X2 = e.GetPosition(this).X;
-                line.Y2 = e.GetPosition(this).Y;
-                currentPoint = e.GetPosition(this);
-                paintSurface.Children.Add(line);
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    Line line = new Line();
+                    SetShapeProperties(line);
+                    line.X1 = currentPoint.X;
+                    line.Y1 = CorrectCurrentY(currentPoint.Y);
+                    line.X2 = e.GetPosition(this).X;
+                    line.Y2 = e.GetPosition(this).Y - SettingsPanel.Height - 5; ;
+                    currentPoint = e.GetPosition(this);
+                    paintSurface.Children.Add(line);
+                }
             }
         }
 
         private void ColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
         {
-            newColor = new SolidColorBrush(_colorPicker.SelectedColor.Value);
+            newColor = new SolidColorBrush(_colorPicker.SelectedColor!.Value);
         }
 
         private void CleanCanvas(object sender, RoutedEventArgs e)
         {
             paintSurface.Children.Clear();
+        }
+
+        private void SetShapeProperties(Shape shape)
+        {
+            shape.Stroke = newColor;
+            shape.StrokeThickness = Convert.ToDouble(StrokeThicknesessComboBox.SelectedItem.ToString());
+        }
+
+        private double CorrectCurrentY(double y)
+        {
+            return y - SettingsPanel.Height - 5;
         }
     }
 }
